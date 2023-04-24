7d13490340ab2769c58a721839f2e5ec78d9770a
Pradeep Singh <91087103+pydeep9026@users.noreply.github.com>
2023-04-24 12:33:10 +0530

    Delete routes directory

--- routes/podcastroutes.js
+++
@@ -1,8 +0,0 @@
-const {createpodcast,getallpodcasts} =require("../controllers/podcastcontroller")
-
-
-const router=require("express").Router()
-
-router.post("/createpodcast",createpodcast);
-router.get('/getallpodcasts',getallpodcasts)
-module.exports=router
--- routes/userroutes.js
+++
@@ -1,9 +0,0 @@
-const {usersignup, userlogin, deleteuser} =require("../controllers/usercontroller")
-
-
-const router=require("express").Router()
-
-router.post("/usersignup",usersignup)
-router.post("/userlogin",userlogin)
-/*router.delete('/:username', deleteuser)*/
-module.exports=router
